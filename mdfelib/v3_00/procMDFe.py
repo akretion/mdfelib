95df922d8a063de48b283b711289ed33c2812ad7
Luis Felipe Mileo <mileo@kmee.com.br>
2018-05-29 02:14:13 -0300

    classes and doc generated with ./generate

--- mdfelib/v3_00/procMDFe.py
+++ mdfelib/v3_00/procMDFe.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Mon May 21 16:57:41 2018 by generateDS.py version 2.29.2.
+# Generated Tue May 29 02:13:18 2018 by generateDS.py version 2.29.2.
 # Python 3.6.5 (default, Apr  1 2018, 05:46:30)  [GCC 7.3.0]
 #
 # Command line options:
