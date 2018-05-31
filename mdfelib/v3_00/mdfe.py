5635efc9377ec65666672ce6920e6548f9761586
Luis Felipe Mileo <mileo@kmee.com.br>
2018-05-31 00:35:36 -0300

    classes and doc generated with ./generate

--- mdfelib/v3_00/mdfe.py
+++ mdfelib/v3_00/mdfe.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Wed May 30 14:48:11 2018 by generateDS.py version 2.29.2.
+# Generated Thu May 31 00:34:58 2018 by generateDS.py version 2.29.2.
 # Python 3.6.5 (default, Apr  1 2018, 05:46:30)  [GCC 7.3.0]
 #
 # Command line options:
